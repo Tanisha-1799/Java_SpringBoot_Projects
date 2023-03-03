bf007e5f91130afb7c3024c2bda90d36feeb1054
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-03-03 14:24:09 +0530

    Created a spring boot application

--- course_api/src/main/java/io_learningjava/springbootquickstart/CourseApiApp.java
+++ course_api/src/main/java/io_learningjava/springbootquickstart/CourseApiApp.java
@@ -1,5 +1,6 @@
 package io_learningjava.springbootquickstart;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //This annotation tells spring boot that this is the starting point of a spring boot application
@@ -8,6 +9,10 @@ public class CourseApiApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//calling a static method to run this spring boot application
+		SpringApplication.run(CourseApiApp.class, args);
+		//This will create a server container and does all the required work that is needed 
+		//to run the application.
 		
 
 	}
