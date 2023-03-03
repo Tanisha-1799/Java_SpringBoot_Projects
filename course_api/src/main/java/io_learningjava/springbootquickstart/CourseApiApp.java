5ce8682bae09137e05364ff4a57bffc29c4b2687
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-03-03 14:26:51 +0530

    Testing my spring boot application

--- course_api/src/main/java/io_learningjava/springbootquickstart/CourseApiApp.java
+++ course_api/src/main/java/io_learningjava/springbootquickstart/CourseApiApp.java
@@ -10,6 +10,8 @@ public class CourseApiApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//calling a static method to run this spring boot application
+		//The class with the main is passed as the first argument
+		//Second argument is the parameters that are to be passed to the main function.
 		SpringApplication.run(CourseApiApp.class, args);
 		//This will create a server container and does all the required work that is needed 
 		//to run the application.
