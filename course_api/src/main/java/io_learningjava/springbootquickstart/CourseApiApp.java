c202990ebcd3efb4e579f254cbc406c178e04d3a
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-03-03 14:15:24 +0530

    making the spring boot application

--- course_api/src/main/java/io_learningjava/springbootquickstart/CourseApiApp.java
+++ course_api/src/main/java/io_learningjava/springbootquickstart/CourseApiApp.java
@@ -1,9 +1,14 @@
 package io_learningjava.springbootquickstart;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//This annotation tells spring boot that this is the starting point of a spring boot application
+@SpringBootApplication
 public class CourseApiApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
