b21043cbe4bc94ecd1b8c30e3e01400c346bad10
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-03-04 13:48:58 +0530

    Displaying topics through the topic controller

--- course_api/src/main/java/io_learningjava/springbootquickstart/topic/TopicController.java
+++ course_api/src/main/java/io_learningjava/springbootquickstart/topic/TopicController.java
@@ -1,14 +1,30 @@
 package io_learningjava.springbootquickstart.topic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+//whatever is returned by the function will be in json form and this happens due to restController annotation
 
 @RestController
 public class TopicController {
 	
 	@RequestMapping("/topics")
-	public String getAllTopics() {
-		return "All Topics";
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+				new Topic("spring","Spring Framework","Spring Framework description"),
+				new Topic("java","Core Java","Core Java description"),
+				new Topic("javascript","Core Javascript","Core Javascript description"),
+				new Topic("react","Basic react JS","Basics of React JS description "),
+				new Topic("angular","Basic Angular JS","Basics of Angular JS description")
+				
+				
+				
+				
+				); 
 		
 	}
 
