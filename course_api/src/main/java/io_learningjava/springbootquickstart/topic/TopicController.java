13e4055fa71fddf0fdf96f541b34d7e92ae46fac
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-03-04 13:30:16 +0530

    Adding topics rest controller to the application

---
+++ course_api/src/main/java/io_learningjava/springbootquickstart/topic/TopicController.java
@@ -0,0 +1,15 @@
+package io_learningjava.springbootquickstart.topic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@RequestMapping("/topics")
+	public String getAllTopics() {
+		return "All Topics";
+		
+	}
+
+}
