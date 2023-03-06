ce8a8d671aa216285aee4de18c2f7c194436374a
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-03-06 20:01:16 +0530

    Creating a REST Api

--- course_api/src/main/java/io_learningjava/springbootquickstart/topic/TopicService.java
+++ course_api/src/main/java/io_learningjava/springbootquickstart/topic/TopicService.java
@@ -1,10 +1,23 @@
 package io_learningjava.springbootquickstart.topic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
 	
-	
+	private List<Topic> topics=Arrays.asList(
+			new Topic("spring","Spring Framework","Spring Framework description"),
+			new Topic("java","Core Java","Core Java description"),
+			new Topic("javascript","Core Javascript","Core Javascript description"),
+			new Topic("react","Basic react JS","Basics of React JS description "),
+			new Topic("angular","Basic Angular JS","Basics of Angular JS description")
+			
+			
+			
+			
+			); 
 
 }
