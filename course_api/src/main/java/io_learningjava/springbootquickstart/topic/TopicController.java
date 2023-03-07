eb170b1fc4da30c3358869f968042ff309088d4a
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-03-07 18:22:57 +0530

    Making a post request method in controller and service class

--- course_api/src/main/java/io_learningjava/springbootquickstart/topic/TopicController.java
+++ course_api/src/main/java/io_learningjava/springbootquickstart/topic/TopicController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +36,8 @@ public class TopicController {
 	
 	//Making a post request
 	@RequestMapping(method=RequestMethod.POST, value="/topics")
-	public void addTopic() {
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
 		
 	}
 	
