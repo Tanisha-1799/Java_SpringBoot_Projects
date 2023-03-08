eee5e8cdb9ec8c1aeeb1e9b8802f540d72210d42
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-03-08 19:12:25 +0530

    adding the method to service class

--- course_api/src/main/java/io_learningjava/springbootquickstart/topic/TopicService.java
+++ course_api/src/main/java/io_learningjava/springbootquickstart/topic/TopicService.java
@@ -35,7 +35,13 @@ public class TopicService {
 	}
 
 	public void updateTopic(String id, Topic topic) {
-		// TODO Auto-generated method stub
+		for(int i=0;i<topics.size();i++) {
+			Topic t=topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
 		
 	}
 	
