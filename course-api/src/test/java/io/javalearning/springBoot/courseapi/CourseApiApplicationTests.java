165f73b3a61f58c6d1233ce16d9034453f640495
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-03-10 21:57:39 +0530

    Making a spring boot project using spring initializer

---
+++ course-api/src/test/java/io/javalearning/springBoot/courseapi/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javalearning.springBoot.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
